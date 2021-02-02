b7e55b3d70d98e30135e25f389ba865f3284ba63
KhusniddinovUz <khusniddinovuz@gmail.com>
2021-02-02 13:52:30 +0500

    Change FoundUser regarding to searchbar

--- src/components/layout/FoundUsers.js
+++ src/components/layout/FoundUsers.js
@@ -6,7 +6,12 @@ const FoundUsers = () => {
   const users = useSelector((state) => state.users.foundusers);
   return (
     <div className='FoundUsers container mb-1'>
-      {users && users.map((user) => <div key={v4()}>{user.username}</div>)}
+      {users &&
+        users.map((user) => (
+          <div className='mb-2' key={v4()}>
+            {user.username}
+          </div>
+        ))}
     </div>
   );
 };
