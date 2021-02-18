f3e04b9e0e1c8f75325e1f8f61104457de784514
KhusniddinovUz <khusniddinovuz@gmail.com>
2021-02-18 11:42:34 +0500

    create store and reducer for comments

---
+++ src/reducers/comments.js
@@ -0,0 +1,14 @@
+import { GET_COMMENT } from '../action/types';
+const initialState = [];
+
+const comments = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_COMMENT:
+      const newState = state.push(action.payload);
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default comments;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,7 +4,15 @@ import errors from './errors';
 import tweet from './tweet';
 import nav from './nav';
 import users from './users';
+import comments from './comments';
 
-const rootReducer = combineReducers({ auth, errors, tweet, nav, users });
+const rootReducer = combineReducers({
+  auth,
+  errors,
+  tweet,
+  nav,
+  users,
+  comments,
+});
 
 export default rootReducer;
