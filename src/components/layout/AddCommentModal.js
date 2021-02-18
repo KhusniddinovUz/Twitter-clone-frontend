72d1aaa744235f2554c0aa4f1ba259c9c096e72f
KhusniddinovUz <khusniddinovuz@gmail.com>
2021-02-18 17:16:11 +0500

    Install react-modal

---
+++ src/components/layout/AddCommentModal.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Modal from 'react-modal';
+
+const AddCommentModal = () => {
+  return <div className='AddCommentModal'></div>;
+};
+
+export default AddCommentModal;
