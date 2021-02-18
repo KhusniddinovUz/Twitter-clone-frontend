e7eb4bfdeb7330d7890cd0fa6e65db45b329da23
KhusniddinovUz <khusniddinovuz@gmail.com>
2021-02-18 11:19:25 +0500

    Render tweet in Specific Tweet

--- src/components/layout/SpecificTweet.js
+++ src/components/layout/SpecificTweet.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import Tweet from './Tweet';
+import { useSelector } from 'react-redux';
 
 const SpecificTweet = (props) => {
   const tweet_id = props.location.pathname.substring(
     12,
     props.location.pathname.length
   );
-  return <div>Hello You're in the {tweet_id} tweet</div>;
+  const tweet = useSelector((state) => {
+    if (state.tweet.tweets.length > 0) {
+      let tweets = state.tweet.tweets.filter(
+        (tweet) => `${tweet.id}` === tweet_id
+      );
+      return tweets.shift();
+    }
+  });
+  console.log(tweet);
+  return <div>{tweet && <Tweet tweet={tweet} />}</div>;
 };
 
 export default withRouter(SpecificTweet);
