45bcbc86d99ab494daff5ff462a08cbf4b1129f8
KhusniddinovUz <khusniddinovuz@gmail.com>
2021-03-15 16:57:11 +0500

    Install and use lazy-loading

--- src/components/NavComponents/Empty.js
+++ src/components/NavComponents/Empty.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import maintenance from './maintenance.jpg';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 const Empty = () => {
   return (
     <div className='Empty text-center'>
       <div>This feature will be added soon!!!</div>
-      <img src={maintenance} alt='Maintenance break' width='400' />
+      <LazyLoadImage
+        effect='blur'
+        src={maintenance}
+        alt='Maintenance break'
+        width='400'
+        height='400'
+      />
     </div>
   );
 };
