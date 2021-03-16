805861c151cb66b44b6e4adf9513a9b922377d36
KhusniddinovUz <khusniddinovuz@gmail.com>
2021-03-16 23:09:40 +0500

    reload user's tweet once all the tweets are changed

--- src/components/NavComponents/Profile.js
+++ src/components/NavComponents/Profile.js
@@ -7,9 +7,10 @@ const Profile = () => {
   const dispatch = useDispatch();
   const auth = useSelector((state) => state.auth);
   const ownTweets = useSelector((state) => state.tweet.owntweets.length);
+  const tweets = useSelector((state) => state.tweet.tweets);
   useEffect(() => {
     dispatch(getOwnTweets());
-  }, [dispatch]);
+  }, [dispatch, tweets]);
   return (
     <div className='Profile'>
       <div className='container p-4 text-center'>
